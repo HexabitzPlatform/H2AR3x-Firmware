1e924b2a1625b468aa7209f31404734efd6c15e4
TITANPC\User <esmaeel.hi@outlook.com>
2021-09-06 15:22:07 +0300

    push user files

---
+++ User/main.c
@@ -0,0 +1,36 @@
+/*
+ BitzOS (BOS) V0.2.5 - Copyright (C) 2017-2021 Hexabitz
+ All rights reserved
+
+ File Name     : main.c
+ Description   : Main program body.
+ */
+/* Includes ------------------------------------------------------------------*/
+#include "BOS.h"
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* Main function ------------------------------------------------------------*/
+
+int main(void){
+
+	Module_Init();		//Initialize Module &  BitzOS
+
+	//Don't place your code here.
+	for(;;){}
+}
+
+/*-----------------------------------------------------------*/
+
+/* User Task */
+void UserTask(void *argument){
+
+	while(1){
+
+
+	}
+}
+
+/*-----------------------------------------------------------*/
