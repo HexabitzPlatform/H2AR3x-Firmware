0d0573e2ad61b71b0e6257285035b0989a16a142
Mahmood_Reda <mr4141705@gmail.com>
2024-06-15 18:46:22 +0300

    Adding Messaging (CODE_H2AR3_SAMPLE_V ,CODE_H2AR3_SAMPLE_V,CODE_H2AR3_STREAM_PORT_V,CODE_H2AR3_STREAM_PORT_A )

--- H2AR3/H2AR3.c
+++ H2AR3/H2AR3.c
@@ -383,6 +383,38 @@ void Module_Peripheral_Init(void){
  */
 Module_Status Module_MessagingTask(uint16_t code,uint8_t port,uint8_t src,uint8_t dst,uint8_t shift){
 	Module_Status result =H2AR3_OK;
+	  uint32_t Numofsamples;
+		  uint32_t timeout;
+
+		switch(code){
+		case CODE_H2AR3_SAMPLE_V:
+		{
+			SampletoPort(cMessage[port-1][shift],cMessage[port-1][1+shift],VOLT);
+			break;
+		}
+
+		case CODE_H2AR3_SAMPLE_A:
+		{
+			SampletoPort(cMessage[port-1][shift],cMessage[port-1][1+shift],AMP);
+			break;
+		}
+		case CODE_H2AR3_STREAM_PORT_V:
+			{
+				Numofsamples = ((uint32_t) cMessage[port - 1][2 + shift] ) + ((uint32_t) cMessage[port - 1][3 + shift] << 8) + ((uint32_t) cMessage[port - 1][4 + shift] << 16) + ((uint32_t)cMessage[port - 1][5 + shift] << 24);
+				timeout = ((uint32_t) cMessage[port - 1][6 + shift] ) + ((uint32_t) cMessage[port - 1][7 + shift] << 8) + ((uint32_t) cMessage[port - 1][8 + shift] << 16) + ((uint32_t)cMessage[port - 1][9 + shift] << 24);
+				Exportstreamtoport(cMessage[port-1][shift] ,cMessage[port-1][1+shift],VOLT, Numofsamples, timeout);
+				break;
+			}
+
+			case CODE_H2AR3_STREAM_PORT_A:
+			{
+				Numofsamples = ((uint32_t) cMessage[port - 1][2 + shift] ) + ((uint32_t) cMessage[port - 1][3 + shift] << 8) + ((uint32_t) cMessage[port - 1][4 + shift] << 16) + ((uint32_t)cMessage[port - 1][5 + shift] <<24);
+				timeout = ((uint32_t) cMessage[port - 1][6 + shift] ) + ((uint32_t) cMessage[port - 1][7 + shift] << 8) + ((uint32_t) cMessage[port - 1][8 + shift] << 16) + ((uint32_t)cMessage[port - 1][9 + shift]<<24);
+				Exportstreamtoport(cMessage[port-1][shift] ,cMessage[port-1][1+shift],AMP, Numofsamples, timeout);
+				break;
+			}
+		}
+		return result;
 
 }
 /* --- Get the port for a given UART. 
